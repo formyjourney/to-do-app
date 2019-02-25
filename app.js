c542a08312129fd9db062a0b3cdfb4bcdc493b46
formyjourney <35152140+formyjourney@users.noreply.github.com>
2019-02-25 14:47:45 -0500

    app.js updated

--- app.js
+++ app.js
@@ -1,44 +1,62 @@
 function onReady() {
-  const toDos = []
   const addToDoForm = document.getElementById('addToDoForm');
+  let toDos = [];
+  let id = 0;
 }
 
 function createNewToDo() {
-  const newToDoText = document.getElementById('newToDoText');
-  if (!newToDoText.value) { return; }
+7	    const newToDoText = document.getElementById('newToDoText');
+8	    if(!newToDoText.value) { return; }
 
-  toDos.push({
-    title: newToDoText.value,
-    complete: false
-  });
-  newToDoText.value='';
+      toDos.push ({
+      title: newToDoText.value,
+      complete: false,
+      id: id
+      });
 
+  id++;
+
+  newToDoText.value = '';
   renderTheUI();
 }
-
-function renderTheUI() {
-  const toDoList = document.getElementbyID('toDoList');
-
-  toDoList.textContent = '';
-
-  toDos.forEach(function(todo) {
-    const newLi = document.createElement('li');
-    const checkbox = document.createElement('input');
-    checkbox.type = "checkbox";
-
-    newLi.textContent = toDo.title;
-
-    toDoList.appendChild(newLi);
-    newLi.appendChild(checkbox);
-  });
-  }
 
 addToDoForm.addEventListener('submit', event => {
   event.preventDefault();
   createNewToDo();
-});
+  });
 
 renderTheUI();
+}
+
+function renderTheUI() {
+    const toDoList = document.getElementById('toDoList');
+
+    toDoList.textContent = '';
+
+    toDos.forEach(function(toDo) {
+      const newLi = document.createElement('li');
+      const checkbox = document.createElement('input');
+      checkbox.type = "checkbox";
+
+      const deleteButton = delete.createElement('button');
+      deleteButton.textcontent = 'Delete!';
+
+      deleteButton.addEventListener('click', event => {
+       toDos = toDos.filter(function(item){
+         return item.id !== toDo.id;
+       })
+
+        renderTheUI();
+      });
+
+      newLi.textContent = toDo.title;
+
+      toDoList.appendChild(newLi);
+      newLi.appendChild(checkbox);
+      newLi.appendChild(deleteButton);
+    })
+
+  }
 
 window.onload = function() {
   onReady();
